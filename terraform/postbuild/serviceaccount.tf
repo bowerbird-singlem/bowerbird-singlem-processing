9dae4d1e47f97944a02b538ba313b71d772bbde9
Mitchell Cunningham <mitchac@gmail.com>
2021-09-03 12:53:52 +1000

    fix service account names

--- terraform/postbuild/serviceaccount.tf
+++ terraform/postbuild/serviceaccount.tf
@@ -1,10 +1,10 @@
 resource "google_service_account" "cloudrun_invoker" {
-  account_id   = "bowerbird_singlem_processing_cloudrun_invoker"
+  account_id   = "bowerbird-singlem-processing-cloudrun-invoker"
   project = var.project
 }
 
 resource "google_service_account" "cloudrun_executor" {
-  account_id   = "bowerbird_singlem_processing_cloudrun_executor"
+  account_id   = "bowerbird-singlem-processing-cloudrun-executor"
   project = var.project
 }
 
