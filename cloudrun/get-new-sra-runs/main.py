1084b3d28760f2ce2304a1ac1879a985f8db4eb3
Mitchell Cunningham <mitchac@gmail.com>
2021-09-03 14:08:46 +1000

    uncomment bigquery code in cloudrun and rerun

--- cloudrun/get-new-sra-runs/main.py
+++ cloudrun/get-new-sra-runs/main.py
@@ -7,18 +7,20 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def index():
-    #client = bigquery.Client()
+    
+    client = bigquery.Client()
 
-    # Perform a query.
-    #QUERY = (
-    #    'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` '
-    #    'WHERE state = "TX" '
-    #    'LIMIT 3')
-    #query_job = client.query(QUERY)  # API request
-    #rows = query_job.result()  # Waits for query to finish
+    QUERY = (
+        'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` '
+        'WHERE state = "TX" '
+        'LIMIT 3')
+    
+    query_job = client.query(QUERY)  # API request
+    
+    rows = query_job.result()  # Waits for query to finish
 
-    #for row in rows:
-    #    print(row.name)
+    for row in rows:
+        print(row.name)
 
     return ("", 204)
 
