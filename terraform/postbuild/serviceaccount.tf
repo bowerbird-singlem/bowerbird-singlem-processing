38af5cc9750d5aa2020113c16fbbae521b470a5d
Mitchell Cunningham <mitchac@gmail.com>
2021-09-03 12:59:40 +1000

    shorten service account ids

--- terraform/postbuild/serviceaccount.tf
+++ terraform/postbuild/serviceaccount.tf
@@ -1,10 +1,10 @@
 resource "google_service_account" "cloudrun_invoker" {
-  account_id   = "bowerbird-singlem-processing-cloudrun-invoker"
+  account_id   = "bb-singlem-proc-cr-invoker"
   project = var.project
 }
 
 resource "google_service_account" "cloudrun_executor" {
-  account_id   = "bowerbird-singlem-processing-cloudrun-executor"
+  account_id   = "bb-singlem-proc-cr-executor"
   project = var.project
 }
 
