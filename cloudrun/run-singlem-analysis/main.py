3578364156eff8cb1f5f0b86e2c0ebd82e3f48e4
Mitchell Cunningham <mitchac@gmail.com>
2021-09-07 10:50:46 +1000

    test lifesciences from cloudrun

--- cloudrun/run-singlem-analysis/main.py
+++ cloudrun/run-singlem-analysis/main.py
@@ -1,5 +1,11 @@
 import os
 import base64
+import json
+import sys
+from pprint import pprint
+
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
 
 from flask import Flask, request
 
@@ -26,6 +32,18 @@ def index():
 
     print(f"SRA Accession: {acc}")
 
+        credentials = GoogleCredentials.get_application_default()
+    service = discovery.build('lifesciences', 'v2beta', credentials=credentials)
+    parent = 'projects/maximal-dynamo-308105/locations/us-central1'
+
+    with open(os.path.join(sys.path[0], "pipeline.json"), "r") as f:
+        run_pipeline_request_body = json.load(f)
+
+    request = service.projects().locations().pipelines().run(parent=parent, body=run_pipeline_request_body)
+    response = request.execute()
+
+    pprint(response)
+    
     return ("", 204)
 
 
