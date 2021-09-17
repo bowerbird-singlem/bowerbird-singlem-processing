b96e1fc1e77acf54cfbbbf2a324313b02e2a4af8
Mitchell Cunningham <mitchac@gmail.com>
2021-09-17 16:25:41 +1000

    pass download method order

--- cloudrun/get-new-sra-runs/main.py
+++ cloudrun/get-new-sra-runs/main.py
@@ -28,6 +28,7 @@ def index():
                 SRA_ACCESSION_NUM=row.acc,
                 MBASES = str(row.mbases),
                 MBYTES = str(row.mbytes),
+                DOWNLOAD_METHOD_ORDER = "aws-http prefetch",
                 TASK_NAME = "singlem",
                 TASK_WORKFLOW_SCRIPT_PATH = "gs://maximal-dynamo-308105-bowerbird/tasks/singlem/pipeline.json",
                 TASK_OUTPUT_PATH = "gs://maximal-dynamo-308105-bowerbird/outputs/singlem",
