a5b9adac17a4e7955b1ef20d1904335e2fa2e378
Mitchell Cunningham <mitchac@gmail.com>
2021-09-29 12:03:46 +1000

    reduce sra accesion file size for testing

--- cloudrun/get-new-sra-runs/main.py
+++ cloudrun/get-new-sra-runs/main.py
@@ -16,7 +16,7 @@ def index():
 
     QUERY = (
         f'SELECT acc, mbases, mbytes FROM `{project_id}.singlem.sra_metadata_test1` '
-        'WHERE mbases < 1000 '
+        'WHERE mbases < 300 '
         'LIMIT 1')
     
     query_job = client.query(QUERY)  # API request
